0cc74dee7115eb39f8bc7adba3ea19535a4eae27
Jake Smith <jake.smith92@gmail.com>
2015-01-16 16:15:17 -0600

    Create migration to add slug column for talks, easier to link speakers to talks in frontend

---
+++ db/migrations/20150115144524_add_talk_slug_to_talks.php
@@ -0,0 +1,46 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddTalkSlugToTalks extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        // Add new slug column
+        $this->table('talks')
+            ->addColumn('slug', 'string')
+            ->save();
+
+        $talks = $this->fetchAll('SELECT * FROM talks');
+
+        // Create slug from initial talk title
+        foreach ($talks as $talk) {
+            $id = $talk['id'];
+            $titleTrimmed = preg_replace(
+                "([^a-zA-Z0-9\s])",
+                "",
+                $talk['title']
+            );
+
+            $slug = strtolower(str_replace(
+                " ",
+                "-",
+                $titleTrimmed
+            ));
+            $query = "UPDATE talks SET slug = \"{$slug}\", updated_at = NOW() WHERE id = {$id}";
+            $this->execute($query);
+        }
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->table('talks')
+            ->removeColumn('slug');
+    }
+}
